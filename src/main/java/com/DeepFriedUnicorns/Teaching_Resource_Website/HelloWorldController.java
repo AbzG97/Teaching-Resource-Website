18be1104d42379dcb9ed50aad5ae2016a618dc3f
AbzG97 <Abdulla.ghazalah@hotmail.com>
2022-02-14 18:13:23 -0500

    updated mappings urls

--- src/main/java/com/DeepFriedUnicorns/Teaching_Resource_Website/HelloWorldController.java
+++ src/main/java/com/DeepFriedUnicorns/Teaching_Resource_Website/HelloWorldController.java
@@ -13,8 +13,8 @@ public class HelloWorldController {
      *
      * @return returns a string to test if the client is able to use the API
      */
-//    @RequestMapping(method = RequestMethod.GET, path = "/HelloWorld")
-    @GetMapping("/HelloWorld")
+    @RequestMapping(method = RequestMethod.GET, path = "/HelloWorld")
+//    @GetMapping("/HelloWorld")
     public String GetHelloWorld(){
         System.out.print("call made from client");
 
